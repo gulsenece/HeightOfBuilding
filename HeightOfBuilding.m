b8b3f770835ee569858af1b652941c1c45da57a8
Gulsen Ece Kilsedar <74204842+gulsenece@users.noreply.github.com>
2023-04-18 17:44:39 +0300

    Add files via upload

---
+++ HeightOfBuilding.m
@@ -0,0 +1,62 @@
+% References
+% Problem is example 5, https://www.kalmanfilter.net/kalman1d.html 
+% Matlab Plotting https://www.mathworks.com/help/matlab/ref/plot.html 
+
+% Define system model
+x_true = 50; %True Height of Buildings
+z = [48.54, 47.11, 55.01, 55.15, 49.89, 40.85, 46.72, 50.05, 51.27, 49.95];% Simulate measurement
+R = 25; % Measurement noise covariance
+
+% Set initial values
+N = length(z);  % Number of measurements
+P = zeros(N, 1);  % Array to store error covariance
+K = zeros(N, 1);  % Array to store Kalman gain
+P_minus = zeros(N, 1);  % Array to store predicted error covariance
+
+% Initialize the first value of P and P_minus
+P(1) = 225; % Initial error covariance
+P_minus(1) = 225; % Initial predicted error covariance
+
+% Initialize the first value of K using the first value of P_minus
+K(1) = P_minus(1)/(P_minus(1) + R); % Initial Kalman gain
+
+% Initialize the state estimate
+x_hat(1) = 60; % Initial state estimate
+
+% Loop over measurements
+for t = 2:N
+    % Measure
+    x = x_hat ; % Simulate state
+    
+    % Predict state and error covariance
+    x_hat_minus(t) = x_hat(t-1); % Predicted state estimate
+    P_minus(t) = P(t-1); % Predicted error covariance
+
+    % Update state estimate and error covariance
+    K(t) = P_minus(t)/(P_minus(t) + R); % Kalman gain
+    x_hat(t) = x_hat_minus(t) + K(t)*(z(t) - x_hat_minus(t)); % Updated state estimate
+    P(t) = (1 - K(t))*P_minus(t); % Updated error covariance
+end
+
+%Plot Measurement data and Estimation data
+figure;
+t = 1:N;
+plot(t, z, 'ko-', 'LineWidth', 1); % Plot measurement data
+hold on; 
+%hold on is used to hold the current plot so that we can add more plots to the same figure.
+plot(t, x_hat(t), 'rsquare-', 'LineWidth', 1); % Estimated value
+hold on;
+plot(t, x_true*ones(N,1), 'g', 'LineWidth', 1); %True value
+hold on;
+plot(1, 60*ones(N,1), '*'); % Initialization value
+xlabel('Measurement Number');
+ylabel('Height(m)');
+legend('Measurement', 'Estimation', 'True Value', 'Initialization');
+title('Kalman Filter for Building Height Estimation');
+
+% Plot Kalman Gain
+figure;
+plot(t, K, 'bo-', 'LineWidth', 1); % Kalman Gain values against the measurement number
+title('Kalman Gain');
+xlabel('Measurement Number');
+ylabel('Kalman Gain');
